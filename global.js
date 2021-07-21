75618164b8ffc0024ce44bbbfaeab2dc7f290700
TopazRN <topazsorowako2@gmail.com>
2021-07-21 07:02:07 +0700

    Create methods to insert, update, and delete db

--- global.js
+++ global.js
@@ -1,4 +1,8 @@
 let db;
+let consoleStyles = {
+  success: "background-color: green; color: white",
+  fail: "background-color: red; color: white",
+};
 
 document.addEventListener('DOMContentLoaded', function () {
   db = new DB();
@@ -26,7 +30,7 @@ class DB {
     let request = objectStore.get(id);
 
     request.onerror = event => {
-      console.log("Unable to retrieve data from database!");
+      console.log("%c Unable to retrieve data from database!", consoleStyles.fail);
     };
 
     request.onsuccess = event => {
@@ -40,11 +44,43 @@ class DB {
     objectStore.openCursor().onsuccess = event => {
       let cursor = event.target.result;
       if (cursor) {
-        data.push({...cursor.value});
+        data.push({ ...cursor.value });
         cursor.continue();
       } else {
         callback(data);
       }
+    };
+  }
+
+  insert(table, data, callback) {
+    let request = this.db.transaction([table], "readwrite")
+      .objectStore(table)
+      .add(data)
+
+    request.onsuccess = event => {
+      console.log(`%c Insert data to table ${table} successful.`, consoleStyles.success);
+      callback()
+    };
+
+    request.onerror = event => {
+      console.log(`%c Insert data to table ${table} unsuccessful.`, consoleStyles.fail);
+    }
+  }
+
+  update(table, id, data, callback) {
+    this.delete(table, id, () => {
+      this.insert(table, data, callback);
+    });
+  }
+
+  delete(table, id, callback) {
+    let request = db.transaction([table], "readwrite")
+    .objectStore(table)
+    .delete(id);
+    
+    request.onsuccess = event => {
+      console.log(`%c Delete data from table ${table} successful.`, consoleStyles.success);
+      callback();
     };
   }
 
@@ -70,16 +106,16 @@ class DB {
 
       request.onsuccess = event => {
         this.db = request.result;
-        console.log("Connected to IndexedDB: " + this.db);
+        console.log("%c Connected to IndexedDB: " + this.db, consoleStyles.success);
       };
 
       request.onerror = event => {
-        console.log("Error connecting to IndexedDB: ");
+        console.log("%c Error connecting to IndexedDB: ", consoleStyles.fail);
       };
 
       request.onupgradeneeded = event => {
         this.db = event.target.result;
-        console.log("Upgrading IndexedDB: " + this.db);
+        console.log("%c Upgrading IndexedDB: " + this.db, consoleStyles.success);
         Object.keys(data).forEach(table => {
           this.db.createObjectStore(table, { keyPath: "id" });
         })
